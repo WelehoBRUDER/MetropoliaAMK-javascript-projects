f9a14d6aa9fe0911b68ab99d63a107cff680e93c
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-04-16 10:07:36 +0300

    digitransit better understood now

--- module4/7_digitransit_routing/script.js
+++ module4/7_digitransit_routing/script.js
@@ -1,13 +1,46 @@
 const query = document.querySelector("#query");
-const form = document.querySelector("form");
+const form = document.querySelector("#form");
 
 form.addEventListener("submit", async (e) => {
 	e.preventDefault();
-	const addressQuery = `address(name: ${query.value}) {
-    id,
-    gid
-    layer,
-    name
+	// const addressQuery = `address(name: ${query.value}) {
+	//   id,
+	//   gid
+	//   layer,
+	//   name
+	// }`;
+	//const address = fetch(`http://api.digitransit.fi/geocoding/v1/search`);
+	const idk = `{
+    plan(
+      from: {lat: 60.168992, lon: 24.932366}
+      to: {lat: 60.175294, lon: 24.684855}
+      numItineraries: 3
+    ) {
+      itineraries {
+        legs {
+          startTime
+          endTime
+          mode
+          duration
+          realTime
+          distance
+          transitLeg
+        }
+      }
+    }
   }`;
-	const address = fetch(`http://api.digitransit.fi/geocoding/v1/search`);
+	const test = await fetch(`https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"digitransit-subscription-key": "6007be3e429c49abacb31e2a20f99398",
+		},
+		body: JSON.stringify({
+			query: idk,
+		}),
+	}).then(async (response) => {
+		const data = await response.json();
+		console.log(data);
+	});
+	//console.log(addressQuery);
 });
