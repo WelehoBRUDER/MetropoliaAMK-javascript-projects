4007a8aa4f63b573a7e57524e805d35c389e5048
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-04-19 09:58:00 +0300

    added time display

--- module4/7_digitransit_routing/script.js
+++ module4/7_digitransit_routing/script.js
@@ -1,5 +1,7 @@
 const query = document.querySelector("#query");
 const form = document.querySelector("#form");
+const stats = document.querySelector(".stats");
+const time = stats.querySelector(".time");
 
 const school = { lat: 60.2237516, lon: 24.7581159 };
 
@@ -19,30 +21,32 @@ form.addEventListener("submit", async (e) => {
 		},
 	}).then(async (response) => {
 		const data = await response.json();
-		return data.features[0].geometry.coordinates;
+		return data.features[0];
 	});
-	console.log(address);
-	const origin = { lat: address[1], lon: address[0] };
+	const name = `${address.properties.label} ${address.properties.postalcode}`;
+	query.value = name;
+	const origin = { lat: address.geometry.coordinates[1], lon: address.geometry.coordinates[0] };
 	const routeToSchool = await getRoute(origin.lat, origin.lon);
-	console.log(routeToSchool);
+	clearMap();
+	displayStats(routeToSchool.data.plan.itineraries[0]);
 	const googleEncodedRoute = routeToSchool.data.plan.itineraries[0].legs;
 	for (let i = 0; i < googleEncodedRoute.length; i++) {
 		let color = "";
 		switch (googleEncodedRoute[i].mode) {
 			case "WALK":
-				color = "green";
-				break;
-			case "BUS":
-				color = "red";
-				break;
-			case "RAIL":
 				color = "cyan";
 				break;
+			case "BUS":
+				color = "blue";
+				break;
+			case "RAIL":
+				color = "orange";
+				break;
 			case "TRAM":
-				color = "magenta";
+				color = "green";
 				break;
 			default:
-				color = "blue";
+				color = "red";
 				break;
 		}
 		const route = googleEncodedRoute[i].legGeometry.points;
@@ -50,6 +54,8 @@ form.addEventListener("submit", async (e) => {
 		L.polyline(pointObjects)
 			.setStyle({
 				color,
+				weight: 4,
+				smoothFactor: 1,
 			})
 			.addTo(map);
 	}
@@ -58,6 +64,46 @@ form.addEventListener("submit", async (e) => {
 		[school.lat, school.lon],
 	]);
 });
+
+// Clears all polyline paths from the map.
+function clearMap() {
+	Object.values(map._layers).forEach((layer) => {
+		if (layer._path !== undefined) {
+			map.removeLayer(layer);
+		}
+	});
+}
+// This function is a hot mess :D
+function displayStats(route) {
+	// First get the legs from route (each transfer)
+	const legs = route.legs;
+
+	// Separate start and end time based on paths (legs)
+	const startTime = new Date(legs[0].startTime);
+	const endTime = new Date(legs[legs.length - 1].endTime);
+
+	// Duration approximation quickly
+	const duration = endTime - startTime;
+
+	// Convert ms to minutes
+	const minutes = duration / 60000;
+
+	// Define start and end time dates (eg. 9.56 --> 11.02)
+	const start = { hours: startTime.getHours(), minutes: startTime.getMinutes() };
+	const end = { hours: endTime.getHours(), minutes: endTime.getMinutes() };
+	// This section is so long because I wanted padding (0) :P
+	const startTimeDate = `${start.hours}.${start.minutes > 9 ? "" : "0"}${start.minutes}`;
+	const endTimeDate = `${end.hours}.${end.minutes > 9 ? "" : "0"}${end.minutes}`;
+
+	// Split minutes into hours and minutes for duration display (kesto)
+	const hours = Math.floor(minutes / 60);
+	const mins = Math.floor(minutes - hours * 60);
+
+	// Finally dump all the info to the user
+	time.textContent = `Alkuaika: ${startTimeDate}, Loppuaika: ${endTimeDate}, Kesto: ${
+		hours > 0 ? hours + "tuntia " : ""
+	}${mins} minuuttia.`;
+}
 
 async function getRoute(lat, lon) {
 	const graphqlQuery = `{
